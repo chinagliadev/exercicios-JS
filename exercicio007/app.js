38372a68a5e6c4bc86b678704c36a244750ad87a
Chinaglia <156460261+chinagliadev@users.noreply.github.com>
2025-09-23 21:13:35 -0300

    exercicios 07

--- exercicio007/app.js
+++ exercicio007/app.js
@@ -1,26 +1,60 @@
 let dataAtual = new Date()
 let dia = dataAtual.getDay()
 
+switch (dia) {
+    case 0:
+        document.getElementById('resposta').innerText = 'Domingo'
+        break;
+    case 1:
+        document.getElementById('resposta').innerText = 'Segunda-feira'
+        break;
+    case 2:
+        document.getElementById('resposta').innerText = 'Terça-feira'
+        break;
+    case 3:
+        document.getElementById('resposta').innerText = 'Quarta-feira'
+        break;
+    case 4:
+        document.getElementById('resposta').innerText = 'Quinta-feira'
+        break;
+    case 5:
+        document.getElementById('resposta').innerText = 'Sexta-feira'
+        break;
+    case 6:
+        document.getElementById('resposta').innerText = 'Sabado'
+        break;
+    default:
+        document.getElementById('resposta').innerText = 'Dia inesxistente'
+        break
+}
 
-if(dia == 0){
+
+
+
+/*
+if (dia == 0) {
     document.getElementById('resposta').innerText = 'Domingo'
-}else if(dia == 1){
+
+} else if (dia == 1) {
     document.getElementById('resposta').innerText = 'Segunda'
 
-}else if(dia == 2){
+} else if (dia == 2) {
     document.getElementById('resposta').innerText = 'Terça-feira'
 
-}else if(dia == 3){
+} else if (dia == 3) {
     document.getElementById('resposta').innerText = 'Quarta-feira'
 
-}else if(dia == 4){
+} else if (dia == 4) {
     document.getElementById('resposta').innerText = 'Quinta-feira'
 
-}else if(dia == 5){
+} else if (dia == 5) {
     document.getElementById('resposta').innerText = 'Sexta-feira'
 
-}else if(dia == 6){
+} else if (dia == 6) {
     document.getElementById('resposta').innerText = 'Sabado'
-}else{
+
+} else {
     document.getElementById('resposta').innerText = ''
+
 }
+*/
